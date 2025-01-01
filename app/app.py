f4017edf3512d4376e64c613ba8d96bfe8c3fd33
Mateusz Drab <mateuszd@live.co.uk>
2025-01-01 18:30:50 +0000

    workaround orange otp by simulating a trusted device

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 import json
 import requests
 import datetime
+import uuid
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app, path=None)
@@ -28,6 +29,10 @@ metric_prepaid_allowance_data_item_expiry_date = Gauge('orangepl_prepaid_allowan
 x_api_key = 'AkWwc1UIBmg049NurALAr7kUdKfgrsqF'
 
 
+def get_full_url(url, params):
+    query_string = '&'.join([f"{key}={value}" for key, value in params.items()])
+    return f"{url}?{query_string}"
+
 @app.get('/metrics')
 @metrics.do_not_track()
 def get_metrics():
@@ -46,102 +51,126 @@ def get_metrics():
 
     # for each account in accounts
     for account in accounts:
-        # login_body variable is a dictionary with login and password from account variable
-        login_body = {'username': account['username'],
-                      'password': account['password'],
-                      'grant_type': 'password',
-                      'scope': 'offline_access',
-                      'client_id': x_api_key,
-                      #   'phone_model': 'iPhone'
-                      }
+        try:            
+            state = str(uuid.uuid4())
+            nonce = str(uuid.uuid4())
 
-        headers = {
-            # 'X-OPL-AppVersion':     '5.72.0.11207',
-            'X-Api-Key':            x_api_key,
-            # 'User-Agent':           'M % C3 % B3j % 20Orange/11207 CFNetwork/1404.0.5 Darwin/22.3.0',
-            'X-OPL-Platform':       'iOS',
-            'X-OPL-RequestSource':  'mobiapp'
-        }
+            body = { "DEVICE_NAME": account["deviceName"]}
 
-        # post request to  https://apiint.orange.pl/app/oauth/v2/token with body as form-data and headers as headers appended to www-form-urlencoded content type header
-        response = requests.post(
-            'https://apiint.orange.pl/app/oauth/v2/token', data=login_body, headers={'Content-Type': 'application/x-www-form-urlencoded', **headers})
+            headers = {
+                'X-Api-Key':            x_api_key,
+                'X-OPL-Platform':       'iOS',
+                'X-OPL-RequestSource':  'mobiapp',
+                'X-OPL-Device': account['device']
+            }
+              
+            params = {
+                'scope': 'openid session identity_customer',
+                'prompt': 'NONE',
+                'response_type': 'code',
+                'state': state,
+                'nonce': nonce,
+                'client_id': 'mobiapp',
+                'offline_token': account['offlineToken'],
+                'login_hint': '%22DEVICE_TOKEN%3D' + account['deviceToken'] + '00A%22'
+            }
 
-        # get access_token from response
-        access_token = response.json()['access_token']
+            # Construct the URL with query parameters
+            url = 'https://apigateway-prd.orange.pl/caap/caaptoken/v1/authorize'
 
-        # print access_token if debug is enabled
-        if app.debug:
-            print(access_token)
+            response = requests.post(get_full_url(url, params), headers=headers, json=body)
 
-        # get account info from /app/oauth/v2/authInfo endpoint using access_token
-        response = requests.get(
-            'https://apiint.orange.pl/app/oauth/v2/authInfo', headers={'Authorization': 'Bearer ' + access_token, **headers})
+            # url https://apigateway-prd.orange.pl/caap/caaptoken/v1/token
+            params = {
+                'grant_type': 'execution_code',
+                'code': response.json()['authenticationId'],
+                'client_id': 'mobiapp',
+                'state': response.json()['state']
+            }
 
-        # set account info to account_info variable
-        account_info = response.json()
+            response = requests.post(get_full_url('https://apigateway-prd.orange.pl/caap/caaptoken/v1/token', params), headers=headers, json={})
 
-        # print debug account_info
-        if app.debug:
-            print(account_info)
+            # get tokens from response
 
-        metric_accounts.labels(
-            username=account['username'], customer_id=account_info['customerId']).set(1)
+            tokens = response.json()
 
-        # get billing accounts from /billingManagement/v2/billingAccounts/briefs?customerId=
-        response = requests.get(
-            'https://apiint.orange.pl/billingManagement/v2/billingAccounts/briefs?customerId=' + account_info['customerId'], headers={'Authorization': 'Bearer ' + access_token, **headers})
+            # print debug tokens
+            if app.debug:
+                print(tokens)
 
-        # set billing accounts to billing_accounts variable
-        billing_accounts = response.json()
+            access_token = tokens['access_token']
 
-        # print debug billing_accounts
-        if app.debug:
-            print(billing_accounts)
+            # get account info
+            response = requests.get(
+                'https://apigateway-prd.orange.pl/moaaManagement/api/v1/customer/full?customerId=' + str(account['customerId']), headers={'Authorization': 'Bearer ' + access_token, **headers})
 
-        # for each billing account in billing_accounts
-        for billing_account in billing_accounts:
-            metric_billing_accounts.labels(
-                username=account['username'], customer_id=account_info['customerId'], billing_account_type=billing_account['billingAccountType'], billing_account_code=billing_account['billingAccountCode'], billing_account_name=billing_account['billingAccountName']).set(1)
-            # if billing account type is mobileprepaid
-            if billing_account['billingAccountType'] == 'mobileprepaid':
-                # get prepaid status from /prepaid/v1/status/{billingAccountCode}
-                response = requests.get(
-                    'https://apiint.orange.pl/prepaid/v1/status/' + billing_account['billingAccountCode'], headers={'Authorization': 'Bearer ' + access_token, **headers})
+            # set account info to account_info variable
+            account_info = response.json()
 
-                # set prepaid status to prepaid_status variable
-                prepaid_status = response.json()
+            # print debug account_info
+            if app.debug:
+                print(account_info)
 
-                # print debug prepaid_status
-                if app.debug:
-                    print(prepaid_status)
+            metric_accounts.labels(
+                username=account['username'], customer_id=account_info['id']).set(1)
 
-                # set prepaid expiry date metric
-                metric_prepaid_expiry_date.labels(
-                    username=account['username'], customer_id=account_info['customerId'], billing_account_code=billing_account['billingAccountCode']).set(datetime.datetime.strptime(prepaid_status['accountExpiryDate'], '%Y-%m-%d %H:%M:%S').timestamp())
+            # get billing accounts
+            response = requests.get(
+                'https://apigateway-prd.orange.pl/moaaManagement/api/v1/accounts?customerId=' + account_info['id'], headers={'Authorization': 'Bearer ' + access_token, **headers})
 
-                # set prepaid balance metrics for gc and pc
-                metric_prepaid_cash.labels(
-                    username=account['username'], customer_id=account_info['customerId'], billing_account_code=billing_account['billingAccountCode'], cash_type='gc').set(int(prepaid_status['gc']['value']['amount'])/100)
-                # check if prepaid_status has key pc
-                if 'pc' in prepaid_status:
+            # set billing accounts to billing_accounts variable
+            billing_accounts = response.json()
+
+            # print debug billing_accounts
+            if app.debug:
+                print(billing_accounts)
+
+            # for each billing account in billing_accounts
+            for billing_account in billing_accounts:
+                metric_billing_accounts.labels(
+                    username=account['username'], customer_id=account_info['id'], billing_account_type=billing_account['type'], billing_account_code=billing_account['code'], billing_account_name=billing_account['name']).set(1)
+                # if billing account type is mobileprepaid
+                if billing_account['type'] == 'mobileprepaid':
+                    # get prepaid status
+                    response = requests.get(
+                        'https://apigateway-prd.orange.pl/wia/api/v1/account/status/' + billing_account['code'], headers={'Authorization': 'Bearer ' + access_token, **headers})
+
+                    # set prepaid status to prepaid_status variable
+                    prepaid_status = response.json()
+
+                    # print debug prepaid_status
+                    if app.debug:
+                        print(prepaid_status)
+
+                    # set prepaid expiry date metric
+                    metric_prepaid_expiry_date.labels(
+                        username=account['username'], customer_id=account_info['id'], billing_account_code=billing_account['code']).set(datetime.datetime.strptime(prepaid_status['accountExpiryDate'], '%Y-%m-%d %H:%M:%S').timestamp())
+
+                    # set prepaid balance metrics for gc and pc
                     metric_prepaid_cash.labels(
-                        username=account['username'], customer_id=account_info['customerId'], billing_account_code=billing_account['billingAccountCode'], cash_type='pc').set(int(prepaid_status['pc']['value']['amount'])/100)
+                        username=account['username'], customer_id=account_info['id'], billing_account_code=billing_account['code'], cash_type='gc').set(int(prepaid_status['gc']['value']['amount'])/100)
+                    # check if prepaid_status has key pc
+                    if 'pc' in prepaid_status:
+                        metric_prepaid_cash.labels(
+                            username=account['username'], customer_id=account_info['id'], billing_account_code=billing_account['code'], cash_type='pc').set(int(prepaid_status['pc']['value']['amount'])/100)
 
-                # set prepaid data metrics for DATA and DATA_R
-                metric_prepaid_allowance_data.labels(
-                    username=account['username'], customer_id=account_info['customerId'], billing_account_code=billing_account['billingAccountCode'], data_type='DATA').set(int(prepaid_status['fractions']['DATA']['sum']['amount']) * 1024)
-                metric_prepaid_allowance_data.labels(
-                    username=account['username'], customer_id=account_info['customerId'], billing_account_code=billing_account['billingAccountCode'], data_type='DATA_R').set(int(prepaid_status['fractions']['DATA_R']['sum']['amount']) * 1024)
+                    # set prepaid data metrics for DATA and DATA_R
+                    metric_prepaid_allowance_data.labels(
+                        username=account['username'], customer_id=account_info['id'], billing_account_code=billing_account['code'], data_type='DATA').set(int(prepaid_status['fractions']['DATA']['sum']['amount']) * 1024)
+                    metric_prepaid_allowance_data.labels(
+                        username=account['username'], customer_id=account_info['id'], billing_account_code=billing_account['code'], data_type='DATA_R').set(int(prepaid_status['fractions']['DATA_R']['sum']['amount']) * 1024)
 
-                # for each item in prepaid_status['fractions']['DATA']['balances'] and prepaid_status['fractions']['DATA_R']['balances']
-                for item in [*prepaid_status['fractions']['DATA']['balances'], *prepaid_status['fractions']['DATA_R']['balances']]:
-                    # set prepaid data item metric
-                    metric_prepaid_allowance_data_item.labels(
-                        username=account['username'], customer_id=account_info['customerId'], billing_account_code=billing_account['billingAccountCode'], data_type=item['type'], description=item['description']).set(int(item['value']['amount']) * 1024)
-                    # set prepaid data item expiry date metric
-                    metric_prepaid_allowance_data_item_expiry_date.labels(
-                        username=account['username'], customer_id=account_info['customerId'], billing_account_code=billing_account['billingAccountCode'], data_type=item['type'], description=item['description']).set(datetime.datetime.strptime(item['expiryDate'], '%Y-%m-%d %H:%M:%S').timestamp())
+                    # for each item in prepaid_status['fractions']['DATA']['balances'] and prepaid_status['fractions']['DATA_R']['balances']
+                    for item in [*prepaid_status['fractions']['DATA']['balances'], *prepaid_status['fractions']['DATA_R']['balances']]:
+                        # set prepaid data item metric
+                        metric_prepaid_allowance_data_item.labels(
+                            username=account['username'], customer_id=account_info['id'], billing_account_code=billing_account['code'], data_type=item['type'], description=item['description']).set(int(item['value']['amount']) * 1024)
+                        # set prepaid data item expiry date metric
+                        metric_prepaid_allowance_data_item_expiry_date.labels(
+                            username=account['username'], customer_id=account_info['id'], billing_account_code=billing_account['code'], data_type=item['type'], description=item['description']).set(datetime.datetime.strptime(item['expiryDate'], '%Y-%m-%d %H:%M:%S').timestamp())
+        except Exception as e:
+            print(e)
+
 
     return metrics_wsgi_app
 
